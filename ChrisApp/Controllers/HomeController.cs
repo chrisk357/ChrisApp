c3cc3040bd98af02e7a0fdec5c7632a9cbd943cc
Chris Desktop <chrisk357@gmail.com>
2019-06-13 02:11:18 -0500

    Add Data with EFCore

---
+++ ChrisApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChrisApp.Models;
+using ChrisApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChrisApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISlimeRepository _slimeRepository;
+        public HomeController(ISlimeRepository slimeRepository)
+        {
+            _slimeRepository = slimeRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            var slimes = _slimeRepository.GetAllSlimes().OrderBy(s => s.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Slime Store",
+                Slimes = slimes.ToList()
+        };
+
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ChrisApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ChrisApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChrisApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Slime> Slimes { get; set; }
+    }
+}
